6b4855e3ffde961649e1731f4be44db609fe185f
Ilija <ilija.beslin@gmail.com>
2022-05-29 20:58:59 +0200

    brat poceo da igra

--- evaluation.py
+++ evaluation.py
@@ -1,5 +1,4 @@
 # from state import State
-
 #Heuristika
 # 1 - Closed Morris: 1 if a morris was closed in the last move by the player
 #     (and an opponent’s piece should be grabbed in this move),
@@ -16,16 +15,60 @@
 # 8 - Winning configuration: 1 if the state is winning for the player, -1 if losing, 0 otherwise
 
 
-# MILL_VAL = 50
-# DOUBLE_MILL_VAL = 100
-# MILL_DIFF_VAL = 20
-# BLOCKED_DIFF_VAL = 20
+def change_player(state,player):
+    if player == state.PLAYER:
+        return state.AI
+    else:
+        return state.PLAYER
+
+def are_there_non_mill_figures(state,player):
+    for i in range(0,24):
+        if not is_mill(state,player,i): #Cim naidje na jednu figuru koja nije u mici prekida pretragu
+            return True
+    return False
+
+
+#HEURISTIKA 1
+#Proverava da li je igrac napravio micu u odnosu na prosli potez
+#Ako je igrac namestio micu vraca 1, ako je protivnik onda vraca -1, u suprotnom 0
+def closed_mill(state, state_before):
+    # enemy = change_player(state,player)
+    
+    mills_before = count_mills(state_before, state.AI)
+    mills_now = count_mills(state, state.AI)
+
+    enemy_mills_before = count_mills(state_before, state.PLAYER)
+    enemy_mills_now = count_mills(state, state.PLAYER)
+
+    if mills_now > mills_before:
+        return 1
+    elif enemy_mills_now > enemy_mills_before:
+        return -1
+    else:
+        return 0
+
+
+#Proverava da li je spojen mill na odredjenoj poziciji 
+def is_mill(state,player,index):       
+    mills = state.MILLS_BY_INDEX
+    possible_mills = mills[index]
+
+    for mill in possible_mills:
+        count = 0
+        for i in mill:
+            if state.get_value(i) == player:
+                count += 1
+        if count == 3: #spojen mill
+            # state.num_of_mills[player] += 1 #Ako je formiran mill povecavam broj millova
+            return True         
+    return False
+
 
 #Broji koliko ima mica odredjeni igrac i vraca tu vrednost
 def count_mills(state, player):
-    count = 0
     mills = 0
     for mill in state.SINGLE_MILLS:
+        count = 0
         for i in mill:
             if state.get_value(i) != "X":
                 if state.get_value(i) == player:
@@ -36,76 +79,119 @@ def count_mills(state, player):
 
 
 
-#Proverava da li je igrac napravio micu u odnosu na prosli potez
-def closed_mill(state, state_before, player):
-    mills_before = count_mills(state_before, player)
-    mills_now = count_mills(state, player)
-    if mills_now > mills_before:
-        return True
-    else:
-        return False
-
-
-#Proverava da li je spojen mill na odredjenoj poziciji 
-def is_mill(state,index):       
-    player = None
-    figure = state.board[index]
-    if figure == "X":
-        return False
-    elif figure == "⚪":
-        player = "AI"
-    elif figure == "⚫":
-        player = "PLAYER"
-
-    mills = state.MILLS_BY_INDEX
-    possible_mills = mills[index]
-    count = 0
-
-    for mill in possible_mills:
-        for node in mill:
-            if state.board[node] == figure:
-                count += 1
-        if count == 3: #spojen mill
-            state.num_of_mills[player] += 1 #Ako je formiran mill povecavam broj millova
-            return True         
-
-    return False
-
+#HEURISTIKA 2
 #Razlika u broju formiranih morissa
-def mill_difference(state):
-    diff =  state.num_of_mills["PLAYER"] - state.num_of_mills["AI"]
-    return diff
+def mill_diff(state):
+        return count_mills(state, state.AI) - count_mills(state, state.PLAYER)
 
+
+#HEURISTIKA 3
 #Broj blokiranih figura - vraca vrednost AI, PLAYER
 def blocked_figures(state):
-    blocked = {"AI" : 0, "PLAYER": 0}
+    blocked = {state.AI : 0, state.PLAYER: 0}
     for i in range(0,24):
-        if state.board[i] != "X":
+        if state.get_value(i) != "X":
             flag = 1    #FLAG da li je blokirana figura (na pocetku stasvljam da jeste)
-            checking_figure = state.board[i]
-            if checking_figure == "⚪":
-                player = "AI"
-            else:
-                player = "PLAYER"
+            checking_figure = state.get_value(i)
 
             for neighbour in state.NEIGHBOURS[i]:
-                if player == "AI":
-                    if state.board[neighbour] != "⚫":
-                        flag = 0
-                        break
-                else:
-                    if state.board[neighbour] != "⚪":
-                        flag = 0
-                        break
+                if state.get_value(neighbour) == "X":
+                    flag = 0
+                    break
 
             if flag == 1:
-                blocked[player] += 1
+                blocked[checking_figure] += 1
 
-    return blocked["AI"], blocked["PLAYER"]
+    return blocked[state.AI], blocked[state.PLAYER]
 
-# def eval(State,index):
-#     if State.phase == 1:
-#         evaluation = is_mill(State,index) + 
+def diff_blocked_figures(state):
+    white_blocked, black_blocked = blocked_figures(state)
+    return black_blocked - white_blocked 
+
+
+#HEURISTIKA 4
+#Razlika u broju figura na tabli
+def figures_diff(state):
+        return state.white_figures - state.black_figures
+
+
+#HEURISTIKA 5
+#Razlika broja konfiguracija od 2 figure
+#(A 2-piece configuration is one to which adding one more piece would close a morris)
+def num_of_2_piece_config(state,player):
+    two_piece_config = 0
+    for mill in state.SINGLE_MILLS:
+        count_free = 0
+        count_player = 0
+        for i in mill:
+            if state.get_value(i) == player:
+                count_player += 1
+            elif state.get_value(i) == "X":
+                count_free += 1
+        if count_player == 2 and count_free == 1:
+            two_piece_config += 1
+    return two_piece_config
+
+def diff_two_piece_config(state):
+    return num_of_2_piece_config(state,state.AI) - num_of_2_piece_config(state,state.PLAYER)
+
+
+#HEURISTIKA 6
+#Razlika broja konfiguracija od 3 figure
+#(A 3-piece configuration is one to which a piece can be added in which one of two ways to close a morris)
+def num_of_3_piece_config(state,player):
+    three_piece_config = 0
+    for mill in state.CONNECTORS:
+        if mill[0] == mill[1] == mill[2] == player:
+            if mill[3] == mill[4] == "X":
+                three_piece_config += 1
+    return three_piece_config
+
+def diff_three_piece_config(state):
+    return num_of_3_piece_config(state,state.AI) - num_of_3_piece_config(state,state.PLAYER)
+    
+
+#HEURISTIKA 7
+#Razlika u broju duplih mica
+def num_of_double_mills(state,player):
+    double_mills = 0
+    for mill in state.DOUBLE_MILLS:
+        flag = 1
+        for i in mill:
+            if state.get_value(i) != player:
+                flag = 0
+        if flag:
+            double_mills += 1
+    return double_mills
+
+def diff_double_mills(state):
+    return num_of_double_mills(state,state.AI) - num_of_double_mills(state,state.PLAYER)
+
+
+#HEURISTIKA 8
+#Pobednicka konfiguracija (1 ako je pobeda, -1 poraz, u suprotnom 0)
+def winning_configuration(state):
+    result,winner = state.is_end()
+    if result:
+        if winner == state.AI:
+            return 1
+        else:
+            return -1
+    else:
+        return 0
+
+def eval(state, state_before, phase):
+
+    if phase == 1:    #18 26 1 6 21 7
+        print(state)
+        evaluation = \
+            18 * closed_mill(state, state_before) + \
+            26 * mill_diff(state) + \
+            1 * diff_blocked_figures(state) + \
+            6 * figures_diff(state) + \
+            21 * diff_two_piece_config(state) + \
+            7 * diff_three_piece_config(state)
+    return evaluation
 
 
 
--- minimax.py
+++ minimax.py
@@ -1,7 +1,12 @@
 from cmath import inf
+from http.client import CONTINUE
 from state import State
 import evaluation
 import copy
+
+DEPTH = 2
+ALPHA = float(-inf)
+BETA = float(inf)
 
 class Game(object):
 
@@ -16,6 +21,7 @@ class Game(object):
     def initialize_game(self):
         self.current_state = State()
         self.phase = 1
+        self.state_before = copy.deepcopy(self.current_state)
         while True:
             user_input = input("Da li želite prvi da igrate?\n(Unesite 'da' ili 'ne'): ")
             if self.check_first_player_input(user_input) == True:
@@ -40,84 +46,172 @@ class Game(object):
         else:
             return State.PLAYER
 
+
+    #----------------------------------------------------------------------- MINIMAX funkcije -----------------------------------------------------------------------
+
     def max(self, depth, alpha, beta):
         max_eval = float(-inf)
+        max_index = None
+        remove_index = None
         result = False
 
         if self.phase != 1:
             result, winner = self.current_state.is_end()
 
         if depth == 0 or result == True:
-            return evaluation.eval(self.current_state)
+            return evaluation.eval(self.current_state, self.state_before, self.phase), max_index, remove_index
 
         for i in range(0,24):
             if self.current_state.get_value(i) == "X":
                 self.state_before = copy.deepcopy(self.current_state)
-                self.current_state.set_value(i, self.player_turn)
+                self.current_state.set_value(i, self.current_state.AI)
+                # print(self.current_state) #OBRISATI KASNIJE
 
-                if evaluation.closed_mill(self.current_state, self.state_before, self.player_turn):
-                    eval = self.min_remove(depth-1,alpha,beta)
+
+                if evaluation.closed_mill(self.current_state, self.state_before) == 1:
+                    eval, index, rem = self.min_remove(depth-1,alpha,beta)
                 else:
-                    eval = self.min(depth-1,alpha,beta)
+                    eval, index, rem = self.min(depth-1,alpha,beta)
 
-                max_eval = max(max_eval,eval)
+                if eval > max_eval:
+                    max_eval = eval
+                    max_index = i
+                    remove_index = index
+
                 alpha = max(alpha, eval)
 
                 self.current_state.set_value(i, "X")
 
             if max_eval >= beta:
-                return max_eval
+                return max_eval, max_index, remove_index
 
-        self = self.change_player(self.player_turn)
-        return max_eval
+        return max_eval, max_index, remove_index
 
-    def max_remove(self, depth, alpha, beta):
 
     def min(self, depth, alpha, beta):
         min_eval = float(+inf)
+        min_index = None
+        remove_index = None
         result = False
 
         if self.phase != 1:
             result, winner = self.current_state.is_end()
 
         if depth == 0 or result == True:
-            return evaluation.eval(self.current_state)
+            return evaluation.eval(self.current_state, self.state_before, self.phase), min_index, remove_index
 
         for i in range(0,24):
             if self.current_state.get_value(i) == "X":
                 self.state_before = copy.deepcopy(self.current_state)
-                self.current_state.set_value(i, self.player_turn)
+                self.current_state.set_value(i, self.current_state.PLAYER)
+                # print(self.current_state)   #OBRISATI KASNIJE
 
-                if evaluation.closed_mill(self.current_state, self.state_before, self.player_turn):
-                    eval = self.max_remove(depth-1,alpha,beta)
+                if evaluation.closed_mill(self.current_state, self.state_before) == -1:
+                    eval, index, rem = self.max_remove(depth-1,alpha,beta)
                 else:
-                    eval = self.max(depth-1,alpha,beta)
+                    eval, index, rem = self.max(depth-1,alpha,beta)
 
-                min_eval = min(min_eval,eval)
+                if eval < min_eval:
+                    min_eval = eval
+                    min_index = i
+                    remove_index = index
                 beta = min(beta, eval)
 
                 self.current_state.set_value(i, "X")
 
             if min_eval <= alpha:
-                return min_eval
+                return min_eval, min_index, remove_index
+                
+        return min_eval, min_index, remove_index
 
-        self = self.change_player(self.player_turn)
-        return min_eval
+#---------------------------------------------------------------------------------------------------------------------------------------------------------------
 
 
-    # def minimax(self, state, depth, alpha, beta, max_player):
+#---------------------------------------------------- MINIMAX funckije koje se pozivaju u slucaju brisanja figure --------------------------------------------
 
-    #     eval = self.current_state.eval()    #TO DO: Namestiti eval funkciju u state-u
+    def max_remove(self, depth, alpha, beta):
+        max_eval = float(-inf)
+        max_index = None
+        remove_index = None
+        result = False
+        # enemy = self.change_player(self.player_turn)
+        free_figures = evaluation.are_there_non_mill_figures(self.current_state,self.current_state.PLAYER)  #Bool vrednost koja pokazuje da li ima figura koje nisu u mici
 
-    #     result, winner = self.current_state.is_end()
-    #     if depth == 0 or result == True:
-    #         return eval
+        if self.phase != 1:
+            result, winner = self.current_state.is_end()
 
-    #     #Ako je na potezu max korisnik
-    #     if max_player:
-    #         max_eval = float(-inf)
+        if depth == 0 or result == True:
+            return evaluation.eval(self.current_state, self.state_before, self.phase), max_index, remove_index
 
-            #--------------------------------------------------------------------------------------------------
+        for i in range(0,24):
+            if self.current_state.get_value(i) == self.current_state.PLAYER:
+                
+                if free_figures and evaluation.is_mill(self.current_state,self.current_state.PLAYER,i): #Ako je figura u mici i ima slobodnih figura preskacemo
+                    continue
+
+                self.state_before = copy.deepcopy(self.current_state)
+                self.current_state.set_value(i, "X")
+                self.current_state.black_figures -= 1
+                # print(self.current_state)   #OBRISATI KASNIJE
+
+                eval, index, rem = self.min(depth-1,alpha,beta)
+
+                if eval > max_eval:
+                    max_eval = eval
+                    max_index = i
+                    remove_index = index
+                alpha = max(alpha, eval)
+
+                self.current_state.set_value(i, self.current_state.PLAYER)
+                self.current_state.black_figures += 1
+
+            if max_eval >= beta:
+                return max_eval, max_index, remove_index
+        return max_eval, max_index, remove_index
+
+
+    def min_remove(self, depth, alpha, beta):
+        min_eval = float(+inf)
+        min_index = None
+        remove_index = None
+        result = False
+        # enemy = self.change_player(self.player_turn)
+        free_figures = evaluation.are_there_non_mill_figures(self.current_state,self.current_state.AI)  #Bool vrednost koja pokazuje da li ima figura koje nisu u mici
+
+        if self.phase != 1:
+            result, winner = self.current_state.is_end()
+
+        if depth == 0 or result == True:
+            return evaluation.eval(self.current_state, self.state_before, self.phase), min_index, remove_index
+
+        for i in range(0,24):
+            if self.current_state.get_value(i) == self.current_state.AI:
+                
+                if free_figures and evaluation.is_mill(self.current_state,self.current_state.AI,i): #Ako je figura u mici i ima slobodnih figura preskacemo
+                    continue
+
+                self.state_before = copy.deepcopy(self.current_state)
+                self.current_state.set_value(i, "X")
+                self.current_state.white_figures -= 1
+                # print(self.current_state)   #OBRISATI KASNIJE
+
+                eval, index, rem = self.max(depth-1,alpha,beta)
+
+                if eval < min_eval:
+                    min_eval = eval
+                    min_index = i
+                    remove_index = index
+                beta = min(beta, eval)
+
+                self.current_state.set_value(i, self.current_state.AI)
+                self.current_state.white_figures -= 1
+
+            if min_eval <= alpha:
+                return min_eval, min_index, remove_index
+                
+        return min_eval, min_index, remove_index
+
+#---------------------------------------------------------------------------------------------------------------------------------------------------------------
 
     def phase_one_player(self):
         print(self.current_state)
@@ -130,7 +224,7 @@ class Game(object):
                 print("Unos nije dobar!")
                 continue
         print(self.current_state)
-        if evaluation.closed_mill(self.current_state, self.state_before, "PLAYER"):
+        if evaluation.closed_mill(self.current_state, self.state_before) == -1:
             while True:
                 user_input = input("Unesite polje sa kojeg želite da skinete figuru: ")
                 try:
@@ -149,8 +243,13 @@ class Game(object):
 
     def phase_one_ai(self):
         self.state_before = copy.deepcopy(self.current_state)
-        self.max()
+        eval, index, remove_index = self.max(DEPTH, ALPHA, BETA)
+        self.current_state.set_value(index, self.current_state.AI)
 
+        if evaluation.is_mill(self.current_state, self.current_state.AI, index):
+            print(self.current_state)
+            self.remove_figure(remove_index)       
+        print(self.current_state)
 
             
     def remove_figure(self,index):
